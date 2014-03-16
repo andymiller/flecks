9efdffd07c05dfc09d8b14c96e51cf8760954501
Andrew Miller <acm@seas.harvard.edu>
2014-03-16 19:39:05 -0400

    factored out multi-dimensional kernels with kron structure, added SMKernel

--- kernel.py
+++ kernel.py
@@ -1,9 +1,79 @@
 import numpy as np
 from scipy.spatial.distance import cdist
+####################################################
+# Kernel classes 
+#   - MultiKronKernel: class that takes advantage of 
+#     kronecker structure in multi dimensional covariance matrices
+#     for optimized inverse, cholesky, multiplications, etc. 
+#     This class strings together multiple 1-d kernels
+#
+#   - Kernel and subclasses: implements simple covariance 
+#     functions for one-d kernels
+####################################################
+
+
+class MultiKronKernel: 
+  """ maintains a list of kernels corresponding to each dimension. 
+  This class assumes that the Kernel over the multi-dimensional space
+  is a tensor product kernel, so it's Gram matrix can only be instantiated 
+  over *regular grids* in each dimension.  This is ideal for a discretized 
+  approximation to a space. """
+  
+  def __init__(self, kernel_names): 
+    self._scale = 1.
+    self._kerns = []
+    for kname in kernel_names:
+      self._kerns.append( Kernel.factory(kname) )
+    #self.set_hypers(hypers)
+  
+  def gram_list(self, hparams, grids): 
+    """ generate a list of gram matrices, one for each dimension, 
+    given the fixed grid """
+    kern_hypers = self._chunk_hypers(hparams)
+    Ks = []
+    for d in range(len(self._kerns)):
+        Kd = self._kerns[d].K( grids[d], grids[d], kern_hypers[d]) + \
+                               np.diag(1e-8*np.ones(len(grids[d])) )
+        Ks.append(Kd)
+    return Ks
+ 
+  def hyper_prior_lnpdf(self, h): 
+    """ prior over hyper parameters for this kernel - 
+    using scale invariant for now """
+    hparams = self._chunk_hypers(h)
+    lls = 0
+    for d in range(len(hparams)):
+      lls += self._kerns[d].prior_lnpdf(hparams[d])
+    return lls
+
+  def hypers(self): 
+    """ returns the hyperparameters associated with each kernel 
+    as a vector """
+    hypers = []
+    for k in self._kerns: 
+        hypers.append( k.hyper_params() )
+    return np.reshape(hypers, (-1,))
+
+  def set_hypers(self, hypers):
+    kern_hypers = self._chunk_hypers(hypers)
+    for k in self._kerns:
+      k.set_hyper_params(kern_hypers)
+
+  def _chunk_hypers(self, hypers): 
+    """ separate out a flattened vector of hyperparameters to a 
+    list of arrays of them (using kernel information) """
+    kern_hypers = []
+    startI = 0
+    for d in range(len(self._kerns)):
+      endI = startI + len(self._kerns[d].hyper_params())
+      kern_hypers.append( hypers[startI:endI] )
+      startI = endI
+    return kern_hypers
+
+
 #
 # collection of 1-d kernels to be used with LGCP 
 #
-
 class Kernel: 
   """ simple model for a kernel - PSD function 
   """
@@ -13,8 +83,9 @@ class Kernel:
   @staticmethod
   def factory(kernel_name = "sqe"): 
     return {
-      'sqe': SQEKernel(), 
-      'per': PerKernel(),
+      'sqe'  : SQEKernel(), 
+      'sqeu' : SQEKernelUnscaled(),
+      'per'  : PerKernel(),
       }.get(kernel_name, SQEKernel())
 
   def K(self, Xi, Xj, hypers=None): 
@@ -26,8 +97,84 @@ class Kernel:
   def prior_lnpdf(self, hypers):
     raise NotImplementedError
 
-class SQEKernel(Kernel): 
+class SpectralMixtureKernel(Kernel):
+  """ Simple, one-dimensional spectral mixture kernel.  The 
+  spectral density of this stationary kernel is represented 
+  by a mixture of Gaussians, which provides a closed form for 
+  the resulting spatial kernel. 
 
+  Refs: 
+   - Wilson and Adams. Gaussian Process Kernels for Pattern 
+     Discovery and Extrapolation. ICML, 2013.
+  """
+  def __init__(self, num_comp = 3):
+    #default weights, default means, default vars for spectral density
+    self._num_comp = num_comp
+    ws  = 1./num_comp * np.ones(num_comp)  # weights
+    mus = np.arange(num_comp)              # means
+    vs  = np.ones(num_comp)                # variances
+    self.set_hyper_params( np.append(ws, np.append(mus, vs)) )
+
+  def K(self, Xi, Xj, hypers=None):
+    assert len(Xi.shape)==len(Xj.shape) and len(Xi.shape)==1, "multi-dim not supported"
+    if hypers is not None:
+      self.set_hyper_params( hypers )
+
+    #pairwise dists
+    tau = np.subtract.outer(Xi, Xj)
+    k = np.zeros( tau.shape )
+    for q in range(self._num_comp): 
+      kq = np.exp(-2.*np.pi*np.pi * tau*tau * self._vars[q]) * \
+           np.cos( 2.*np.pi * tau * self._means[q] )
+      kq *= self._weights[q]
+      k += kq
+    return k
+
+  def hyper_params(self):
+    """ return flat vector of hyper parameters """
+    return np.append( self._weights, np.append(self._means, self._vars) )
+
+  def set_hyper_params(self, hypers): 
+    """ pass in flat vector of hyper parameters """
+    assert len(hypers) == self._num_comp*3, "SMKernel, num hypers not correct"
+    self._weights, self._means, self._vars = np.split(hypers, 3)
+
+  def prior_lnpdf(self, hypers): 
+    """ constant jeffrey's/scale-free prior on length scales """
+    return -np.sum(np.log(hypers))
+
+
+
+class SQEKernelUnscaled(Kernel):
+  """ SQE Kernel (only one dimension )with only one 
+  hyper paramter, the length scale """
+  def __init__(self, length_scale=1):
+    self._length_scale = length_scale
+  
+  def K(self, Xi, Xj, length_scale=None): 
+    """ gram mat between Xi and Xj """
+    if length_scale is not None:
+      self._length_scale = length_scale
+    
+    # reshape to make sure they are stacks of vecs
+    if len(Xi.shape) == 1:
+      Xi = np.reshape( Xi, (-1, 1) )
+      Xj = np.reshape( Xj, (-1, 1) )
+    dists = cdist(Xi, Xj, 'sqeuclidean')
+    return np.exp( -(1./(2.*self._length_scale*self._length_scale)) * dists)
+ 
+  def hyper_params(self):
+    return np.array([self._length_scale])
+
+  def set_hyper_params(self, hypers):
+    self._length_scale = hypers[0]
+
+  def prior_lnpdf(self, hypers): 
+    """ jeffrey's/scale-free prior on length scales """
+    return -np.sum(np.log(hypers))
+
+
+class SQEKernel(Kernel): 
   """ Simple squared exponential kernel (independent length scales)"""
   def __init__(self, length_scale=1, sigma2=1):
     hyps = np.append(length_scale, sigma2)
@@ -70,8 +217,6 @@ class SQEKernel(Kernel):
   def prior_lnpdf(self, hypers): 
     """ jeffrey's/scale-free prior on length scales """
     return -np.sum(np.log(hypers))
-
-
 
 class PerKernel(Kernel):
   """ Periodic Kernel """
--- lgcp.py
+++ lgcp.py
@@ -1,7 +1,7 @@
 import numpy as np
 from ess import elliptical_slice
 from util import nd_grid_centers, whitened_mh, x_grid_centers
-from kernel import Kernel, SQEKernel
+from kernel import MultiKronKernel #Kernel, SQEKernel
 from kron_util import kron_mat_vec_prod
 from mpl_toolkits.mplot3d.axes3d import Axes3D
 
@@ -23,7 +23,7 @@ from mpl_toolkits.mplot3d.axes3d import Axes3D
 
 class LGCP: 
 
-  def __init__(self, dim=2, grid_dim=(50,50), bbox=[(0,1), (0,1)], covar=["sqe", "sqe"]):
+  def __init__(self, dim=2, grid_dim=(50,50), bbox=[(0,1), (0,1)], kern=None):
     self._dim      = dim              # dimensionality of the point process
     self._grid_dim = grid_dim         # number of tiles for each dimension
     self._bbox     = bbox             # range of each dimension
@@ -31,10 +31,16 @@ class LGCP:
     self._sigma_chol = None
     assert self._dim == len(self._grid_dim), 'LGCP Space dimensions not coherent'
 
+    #compute cell volume
+    self._cell_vol = 1.0
+    for d in range(self._dim):
+      self._cell_vol *= float(bbox[d][1] - bbox[d][0]) / grid_dim[d] 
+
     #set up kernel for each dimension 
-    self._kerns = []
-    for d in range(self._dim): 
-        self._kerns.append( Kernel.factory(covar[d]) )
+    if kern is None: 
+      self._kern = MultiKronKernel( ["sqeu", "sqeu"] )
+    else: 
+      self._kern = Kern
   
   def fit(self, data, Nsamps=550, verbose=True): 
     """ data should come in as a dim by N dimensional numpy array """
@@ -53,7 +59,7 @@ class LGCP:
 
     # sample from the posterior w/ ESS, slice sample Hypers
     z_curr  = np.zeros( Nz )                    # current state of latent GP (Z)
-    h_curr  = self._kern_hypers()               # current values of hyper params
+    h_curr  = self._kern.hypers()               # current values of hyper params
     ll_curr = None                              # current log likelihood of obs
 
     # keep track of all samples
@@ -73,32 +79,25 @@ class LGCP:
       
       ## whitens latent field (probably should find better place for this)
       def whiten(th, f): 
-        Ks = self._gram_list(th)
+        Ks = self._kern.gram_list(th, self._grids)
         Ls = [np.linalg.cholesky(K) for K in Ks]
         Ls_inverse = [np.linalg.inv(L) for L in Ls]
         nu = kron_mat_vec_prod(Ls_inverse, f)
         return nu
 
       def unwhiten(thp, nu): 
-        Ks = self._gram_list(thp)
+        Ks = self._kern.gram_list(thp, self._grids)
         Ls = [np.linalg.cholesky(K) for K in Ks]
         fp = kron_mat_vec_prod(Ls, nu)
         return fp
 
-      def hyper_prior_lnpdf(h): 
-        hparams = self._chunk_hypers(h)
-        lls = 0
-        for d in range(len(hparams)):
-          lls += self._kerns[d].prior_lnpdf(hparams[d])
-        return lls
-
       #sample hyper params
       h_curr, z_hyper, accepted, ll = whitened_mh( h_curr, \
-                                                   z_curr[1:], \
-                                                   whiten, \
-                                                   unwhiten, \
-                                                   lambda(z): self._log_like(np.append(z_curr[0], z)), \
-                                                   hyper_prior_lnpdf )
+               z_curr[1:], \
+               whiten, \
+               unwhiten, \
+               lambda(z): self._log_like(np.append(z_curr[0], z)), \
+               lambda(h): self._kern.hyper_prior_lnpdf(h) )
       z_curr = np.append(z_curr[0], z_hyper)
     
       #store samples
@@ -110,56 +109,37 @@ class LGCP:
       accept_rate += accepted
 
     print "final acceptance rate: ", float(accept_rate)/Nsamps
-    self._z_samps = z_samps[100:,]
-    self._h_samps = h_samps[100:,]
+    self._z_samps = z_samps[300:,]
+    self._h_samps = h_samps[300:,]
     self._lls     = lls
     return z_samps, h_samps
 
   def posterior_mean_lambda(self): 
     """ return the posterior mean lambda surface """
     zvecs  = (self._z_samps[:,0] + self._z_samps[:,1:].T).T
-    lamvec = np.exp(zvecs).mean(axis=0)
+    lamvec = self._cell_vol * np.exp(zvecs).mean(axis=0)
     return np.reshape(lamvec, self._grid_dim, order='F')
 
   def posterior_var_lambda(self):
     """ return the posterior variance of the lambda surface """
     zvecs = (self._z_samps[:,0] + self._z_samps[:,1:].T).T
-    lamvec = np.exp(zvecs).var(axis=0)
+    lamvec = self._cell_vol * np.exp(zvecs).var(axis=0)
     return np.reshape(lamvec, self._grid_dim, order='F')
 
-  def plot_3d(self):
-    X,T = np.meshgrid(self._grids[0], self._grids[1])
-    Z   = self.posterior_mean_lambda()
-    fig = plt.figure(figsize=(14,6))
-    # `ax` is a 3D-aware axis instance because of the projection='3d' keyword argument to add_subplot
-    ax = fig.add_subplot(1, 1, 1, projection='3d')
-    # surface_plot with color grading and color bar
-    p = ax.plot_surface(X, T, Z, rstride=1, cstride=1, linewidth=0, antialiased=False)
-    cb = fig.colorbar(p, shrink=0.5)
-
-  def _kern_hypers(self): 
-    """ returns the hyperparameters associated with each kernel 
-    as a vector """
-    hypers = []
-    for d in range(self._dim): 
-        hypers.append( self._kerns[d].hyper_params() )
-    return np.reshape(hypers, (-1,))
-
-  def _chunk_hypers(self, hypers): 
-    """ separate out a flattened vector of hyperparameters to a 
-    list of arrays of them (using kernel information) """
-    kern_hypers = []
-    startI = 0
-    for d in range(len(self._kerns)):
-      endI = startI + len(self._kerns[d].hyper_params())
-      kern_hypers.append( hypers[startI:endI] )
-      startI = endI
-    return kern_hypers
+  #def plot_3d(self):
+  #  X,T = np.meshgrid(self._grids[0], self._grids[1])
+  #  Z   = self.posterior_mean_lambda()
+  #  fig = plt.figure(figsize=(14,6))
+  #  # `ax` is a 3D-aware axis instance because of the projection='3d' keyword argument to add_subplot
+  #  ax = fig.add_subplot(1, 1, 1, projection='3d')
+  #  # surface_plot with color grading and color bar
+  #  p = ax.plot_surface(X, T, Z, rstride=1, cstride=1, linewidth=0, antialiased=False)
+  #  cb = fig.colorbar(p, shrink=0.5)
 
   def _log_like(self, th): 
     """ approximate log like is just independent poissons """
     loglam = th[0] + th[1:]
-    lam     = np.exp(loglam)
+    lam     = self._cell_vol * np.exp(loglam)
     #pr(x | lam) = lam^x * exp(-lam) / x!
     ll = loglam * self._grid_counts - lam  # log-like
     return np.sum(ll)
@@ -167,34 +147,22 @@ class LGCP:
   def _gen_prior(self, hparams):
     """ generate from the GP prior (with the object's grid points, and 
     some flattened vector of hyperparameters for the list of covariance funcs """
-    Ks = self._gram_list(hparams)             # covariance mats for each dimension
-    Ls = [np.linalg.cholesky(K) for K in Ks]  # cholesky of each cov mat
+    Ks = self._kern.gram_list(hparams, self._grids)  # covariance mats for each dimension
+    Ls = [np.linalg.cholesky(K) for K in Ks]         # cholesky of each cov mat
 
     #generate spatial component and bias 
     z_spatial = kron_mat_vec_prod( Ls, np.random.randn(len(self._grid_counts)) )
     z_bias    = np.sqrt(10)*np.random.randn()
     return np.append(z_bias, z_spatial)
 
-  def _gram_list(self, hparams): 
-    """ generate a list of gram matrices, one for each dimension, 
-    given the fixed grid """
-    kern_hypers = self._chunk_hypers(hparams)
-    Ks = []
-    for d in range(len(self._kerns)):
-        Kd = self._kerns[d].K(self._grids[d], self._grids[d], kern_hypers[d]) + \
-             np.diag(1e-6*np.ones(len(self._grids[d])))
-        Ks.append(Kd)
-    return Ks
-     
 
 if __name__=="__main__":
   
   import pylab as plt
   #x = np.random.rand(500,2)
-  x = np.row_stack( (np.random.beta(a=8, b=2, size=(500,2)), 
-                     np.random.beta(a=2, b=8, size=(500,2)) ) )
-  
-  x[500:,0] = 1.0 - x[500:,0]
+  x = np.row_stack( (np.random.beta(a=5, b=2, size=(500,2)), 
+                     np.random.beta(a=2, b=5, size=(500,2)) ) )
+  #x[500:,0] = 1.0 - x[500:,0]
 
   lgcp = LGCP()
   z_samps = lgcp.fit(x)
@@ -204,7 +172,7 @@ if __name__=="__main__":
   lam_mean = lgcp.posterior_mean_lambda()
   
   fig = plt.figure()
-  plt.imshow(lam_mean, interpolation='none', origin='lower', extent=[0,1,0,1])
+  plt.imshow(lam_mean.T, interpolation='none', origin='lower', extent=[0,1,0,1])
   plt.colorbar()
   plt.hold(True)
   plt.scatter(x[:,0], x[:,1])
